7c5c9d53662be8b5698640ce321cd86d36c3e552
julydeath <manojkarajada.mk@gmail.com>
2023-06-20 00:58:25 +0530

    first commit

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,12 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/manoj/Desktop/car-rent/app/layout.tsx": [
+      "/Users/manoj/Desktop/car-rent/app/globals.css"
+    ]
+  },
+  "cssModules": {
+    "/Users/manoj/Desktop/car-rent/app/page": [
+      "/Users/manoj/Desktop/car-rent/app/globals.css"
+    ]
+  }
+}
